392c94542c492334f77d21d81c96373ecdbc7b1b
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-31 19:47:08 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ def initialize(email)
 end 
 
 def parse
-  email_array = @email.split(/[, ]/).uniq 
+  email_array = @emails.split(/[, ]/).uniq 
   email_array.reject! {|element| element.empty?}
   email_array
 end 
